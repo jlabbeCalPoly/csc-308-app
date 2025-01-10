ede81f0969f57f51d8139572d907ca5d41d156b5
Julian Labbe <jlabbe@calpoly.edu>
2025-01-09 20:47:13 -0800

    Final edits to Form.jsx and MyApp.jsx

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState({
     name: "",
     job: "",
@@ -10,6 +10,11 @@ function Form() {
     const { name, value } = event.target;
     if (name === "job") setPerson({ name: person["name"], job: value });
     else setPerson({ name: value, job: person["job"] });
+  }
+
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({ name: "", job: "" });
   }
   return (
     <form>
@@ -24,11 +29,12 @@ function Form() {
       <label htmlFor="job">Job</label>
       <input
         type="text"
-        job="job"
+        name="job"
         id="job"
         value={person.job}
         onChange={handleChange}
       />
+      <input type="button" value="Submit" onClick={submitForm} />
     </form>
   );
 }
