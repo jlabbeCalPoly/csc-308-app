928539d4af2406adf68fe87585d2760dd720d682
Julian Labbe <jlabbe@calpoly.edu>
2025-01-15 10:53:25 -0800

    Added findUserById function

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,6 +35,9 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/users", (req, res) => {
@@ -45,6 +48,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found");
+  } else {
+    res.send(result);
   }
 });
 
