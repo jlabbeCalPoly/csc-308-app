fe6eb40c5dd4ceaf97f39546816784d378b9d2c8
Julian Labbe <jlabbe@calpoly.edu>
2025-01-19 19:07:15 -0800

    Added POST functionality

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,19 +14,36 @@ function MyApp() {
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
-    return promise
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
   }
 
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
       .then((json) => setCharacters(json["users_list"]))
-      .catch((error) => {console.log(error);});
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
